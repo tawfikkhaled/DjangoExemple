4f775d1524935310e51129cc4b8abdb8290b5405
Your Name <you@example.com>
2021-04-03 00:56:44 +0200

    defining postgresql as backend

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_admin',
+        'USER' : 'postgres',
+        'PASSWORD' : 'postgres',
+        'HOST' : 'vfr-test-01',
+        'PORT': '5432'
     }
 }
 
